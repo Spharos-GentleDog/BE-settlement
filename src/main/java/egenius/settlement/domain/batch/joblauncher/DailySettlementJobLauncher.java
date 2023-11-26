0826915eacc9fbb020897c34b1bd4baf0cf2e1ed
Hwan0518 <qip0518@naver.com>
2023-11-26 19:31:04 +0900

    refactor: 일일 정산 Batch를 스케쥴러로 자동화

--- src/main/java/egenius/settlement/domain/batch/joblauncher/DailySettlementJobLauncher.java
+++ src/main/java/egenius/settlement/domain/batch/joblauncher/DailySettlementJobLauncher.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +18,9 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
-@RestController
-@RequestMapping("/api/v1/settlement/daily/batch")
+//@RestController
+//@RequestMapping("/api/v1/settlement/daily/batch")
+@Service
 @RequiredArgsConstructor
 @Slf4j
 public class DailySettlementJobLauncher {
@@ -28,8 +31,11 @@ public class DailySettlementJobLauncher {
 
 
     //todo: 스케쥴러로 작동하게 만들기
-    @GetMapping("")
-    public BaseResponse paymentDataTransferBatch() {
+//    @GetMapping("")
+    @Scheduled(cron = "${spring.scheduler.daily_settlement_job_launcher.cron}",
+            zone = "${spring.scheduler.daily_settlement_job_launcher.zone}")
+//    public BaseResponse paymentDataTransferBatch() {
+    public void DailySettlementJobLauncher() {
         // '시작 날짜' = '오늘', '끝 날짜' = '내일'
         LocalDateTime start = LocalDate.now().atStartOfDay();
         LocalDateTime end = LocalDate.now().plusDays(1).atStartOfDay();
@@ -58,12 +64,16 @@ public class DailySettlementJobLauncher {
         // 저장된 결제 데이터로 일일 정산을 하는 Job
         try {
             JobExecution jobExecution = jobLauncher.run(dailySettlementJob, jobParameters);
-
+            if (jobExecution.getExitStatus().equals(ExitStatus.COMPLETED)) {
+                log.info("job success");
+            } else {
+                log.info("job failed");
+                log.info("fail code: " + jobExecution.getExitStatus());
+            }
         } catch (Exception e) {
             log.error("dailySettlementJob Failed: "+e.getMessage());
             throw new BaseException(BaseResponseStatus.DAILY_SETTLEMENT_FAILED);
         }
-        return new BaseResponse();
     }
 
 }
